74940aeaa32fce41b7b97fd422ac840d790a22d7
NikolaiChernichenko <nikolai@boylesoftware.com>
2017-02-14 02:11:40 +0200

    Added task for png-sprites generating

---
+++ gulp/tasks/sprite.png.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('sprite:png', function () {
+    var spriteData = $.gulp.src('./source/images/sprite/*.png')
+      .pipe($.gp.spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+      }));
+
+    var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+      .pipe($.gulp.dest($.config.root + '/assets/img'));
+
+    var cssStream = spriteData.css
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+      .pipe($.gulp.dest('./source/style'));
+
+    return spriteData;
+  })
+};
